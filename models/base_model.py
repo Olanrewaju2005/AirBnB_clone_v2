90de3d7b4fd3b35ba15dda287dd1ae313e029216
Olanrewaju2005 <davidolanrewaju04@gmail.com>
2024-03-20 17:28:16 +0100

    DBStorage - States and Cities

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 from datetime import datetime
 
+Base = declarative_base()
 
-class BaseModel:
+class BaseModel():
     """A base class for all hbnb models"""
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         from models import storage
---
+++ models/engine/db_storage.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+from sqlalchemy import create_engine
+
+
+class DBStorage:
+    """
+
+    """
+    __engine = None
+    __session = None
+
+    def __init__(self) -> None:
+
+        username = getenv("HBNB_MYSQL_USER")
+        password = getenv("HBNB_MYSQL_PWD")
+        host = getenv("HBNB_MYSQL_HOST")
+        db_name = getenv("HBNB_MYSQL_DB")
+        db_url = 'mysql+mysqldb://{}:{}@{}/{}'.format(username, password, host, db_name)
+
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(db_url, pool_pre_ping=True)
+
+        if getenv("HBNB_ENV") = dev:
+            Base.metadata.drop_all(self.__engine)
