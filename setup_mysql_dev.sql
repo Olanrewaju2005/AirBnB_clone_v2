9541072c723464011fa2b828459dbf4f2a529516
Olanrewaju2005 <davidolanrewaju04@gmail.com>
2024-03-20 15:00:40 +0100

    MySQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,16 @@
 -- script that prepares a MySQL server for the project
 
+-- create database hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- create new user hbnb_dev (in localhost)
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- hbnb_dev should have all privileges on the database hbnb_dev_db
 GRANT ALL PRIVILEGES  ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- hbnb_dev should have SELECT privilege on the database performance_schema
 GRANT SELECT ON performance_schema TO 'hbnb_dev'@'localhost';
+
+-- flush privileges to apply changes
+FLUSH PRIVILEGES;
