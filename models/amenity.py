360fb898817c617fdefeb56288a6d7f994e0a700
Olanrewaju2005 <davidolanrewaju04@gmail.com>
2024-03-23 14:07:33 +0100

    project completion

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from sqlalchemy.ext.declarative import declarative_base
+from models.place import place_amenity
 
-
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """ This is the class for amenity """
+    __tablename__ = "amenties"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
